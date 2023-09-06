d88f832eda2640173f55eb81620c532df2ffe28d
Luis Restrepo <luchoposada555@gmail.com>
2023-09-06 01:10:36 -0500

    Solucion de conflictos

--- script/script.js
+++ script/script.js
@@ -10,14 +10,3 @@ iniciarSesion.addEventListener("click" , function(e) {
   e.preventDefault(); // Evita el comportamiento predeterminado del botón
   document.querySelector(".inicio_sesion").style.display = "block";
 });
-
-<<<<<<< HEAD
-registrarse.addEventListener("click" , function(event) {
-  event.preventDefault();
-  document.querySelector(".formulario_regis").style.display= "block";
-});
-=======
-
-
->>>>>>> eb06023b44883f0d7d89b397b59c7ebbad1c0f1f
-
