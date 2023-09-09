3831beabe45cbdbcb0eab1047d707d9d115184b6
AngieDiaz-21 <diazangie210@gmail.com>
2023-09-09 03:47:27 -0500

    conversacion whatsapp

--- script/mensajes.js
+++ script/mensajes.js
@@ -8,47 +8,60 @@
     // Respuestas predefinidas para el Usuario 2
     const respuestasUsuario2 = [
         '¡Hola! ¿Como estas?',
-        '¡Muy bien y tu?',
-        'Me alegra',
+        '¡Muy bien, gracias por preguntar',
+        'No, para donde nos vamosNo, para donde nos vamos',
 
     ];
   
     sendButton.addEventListener('click', () => {
       const messageText = messageInput.value.trim();
-  
-      if (messageText !== '') {
-        // Crear un nuevo mensaje del Usuario 1
-        const user1MessageElement = document.createElement('div');
-        user1MessageElement.classList.add('message');
-        user1MessageElement.textContent = `${currentUser}: ${messageText}`;
-        messagesContainer.appendChild(user1MessageElement);
-  
-        // Limpiar el campo de entrada
-        messageInput.value = '';
-  
-        // Simular una respuesta del Usuario 2 después de un breve retraso
-        setTimeout(() => {
-        const respuestaAleatoria =
-        respuestasUsuario2[Math.floor(Math.random() * respuestasUsuario2.length)];
-        const user2MessageElement = document.createElement('div');
-        user2MessageElement.classList.add('message');
-        user2MessageElement.textContent = 'Usuario 2: ' + respuestaAleatoria;
-        messagesContainer.appendChild(user2MessageElement);
-          // Desplazarse hacia abajo para ver el nuevo mensaje
-          messagesContainer.scrollTop = messagesContainer.scrollHeight;
-        }, 1000);
-  
-        // Cambiar al Usuario 2 para el siguiente mensaje
-        currentUser = 'Usuario 1';
-      }
-    });
-  
-    // Manejar el evento Enter para enviar un mensaje
-    messageInput.addEventListener('keydown', (e) => {
-      if (e.key === 'Enter') {
-        e.preventDefault();
-        sendButton.click();
-      }
-    });
-  });
- 
+
+        if (messageText !== '') {
+            // Crear un nuevo mensaje del Usuario 1
+            const user1MessageElement = document.createElement('div');
+            user1MessageElement.classList.add('message');
+            user1MessageElement.textContent = `${currentUser}: ${messageText}`;
+            messagesContainer.appendChild(user1MessageElement);
+          
+            // Limpiar el campo de entrada
+            messageInput.value = '';
+          
+            // Verificar si el mensaje es "Hola"
+            if (messageText.toLowerCase() === 'hola') {
+              // Si es "Hola", responder con "Hola, ¿cómo estás?"
+              setTimeout(() => {
+                const user2MessageElement = document.createElement('div');
+                user2MessageElement.classList.add('message');
+                user2MessageElement.textContent = 'Usuario 2: ¡Hola! ¿Cómo estás?';
+                messagesContainer.appendChild(user2MessageElement);
+                messagesContainer.scrollTop = messagesContainer.scrollHeight;
+              }, 1000);
+            } else if (messageText.toLowerCase() === 'bien' && respuestaUsuario2.toLowerCase() === 'muy bien y tú?') {
+              // Si el mensaje del Usuario 1 es "Bien" y el mensaje del Usuario 2 es "¡Muy bien y tú?",
+              // entonces responder con "¡Muy bien gracias!"
+              setTimeout(() => {
+                const user2MessageElement = document.createElement('div');
+                user2MessageElement.classList.add('message');
+                user2MessageElement.textContent = 'Usuario 2: ¡Muy bien gracias!';
+                messagesContainer.appendChild(user2MessageElement);
+                messagesContainer.scrollTop = messagesContainer.scrollHeight;
+              }, 1000);
+            } 
+            else {
+              // Si no es ninguna de las condiciones anteriores, responder de manera aleatoria como antes
+              setTimeout(() => {
+                const respuestaAleatoria =
+                  respuestasUsuario2[Math.floor(Math.random() * respuestasUsuario2.length)];
+                const user2MessageElement = document.createElement('div');
+                user2MessageElement.classList.add('message');
+                user2MessageElement.textContent = 'Usuario 2: ' + respuestaAleatoria;
+                messagesContainer.appendChild(user2MessageElement);
+                messagesContainer.scrollTop = messagesContainer.scrollHeight;
+              }, 1000);
+            }
+          
+            // Cambiar al Usuario 2 para el siguiente mensaje
+            currentUser = 'Usuario 1';
+          }
+          // ...
+        })})
